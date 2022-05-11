079ac3a5a4663647c9955f0dd7a5bdaeeffc9101
Andreza Dias <61808213+AndrezzaDias@users.noreply.github.com>
2022-05-10 21:30:40 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoGoogle/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjetoGoogle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjetoGoogle")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6ab477d6-8887-41cd-90bf-351151afc34d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProjetoGoogle/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace ProjetoGoogle
+{
+    [TestClass]
+    public class UnitTest1{
+
+
+
+        [TestMethod]
+        public void TestMethod1(){
+
+            By gmail = By.Name("user");
+            By senha = By.Id("pass");
+            By logar = By.Name("commit");
+            By botao = By.Id("menuBotao");
+            
+
+            WebDriver webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+
+            webDriver.Navigate().GoToUrl("https://portal.ucsal.br/");
+
+            //Escrevendo o email 
+            webDriver.FindElement(gmail).SendKeys("Matricula");
+            //Colocando a senha 
+            webDriver.FindElement(senha).SendKeys("senha");
+            //
+            webDriver.FindElement(logar).Click();
+
+            webDriver.FindElement(botao).Click();
+
+
+
+
+        }
+    }
+}
